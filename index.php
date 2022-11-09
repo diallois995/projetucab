29831902ab6d862ba5cb69a4ba015327c13d86af
ASTOU NDOUR <astoumoubayam@gmail.com>
2022-11-09 22:10:53 +0000

    ajout de index php

---
+++ index.php
@@ -0,0 +1,76 @@
+ <!-- ======= Home Section ======= -->
+ <?php
+      require_once("view/partials/extract_vitrine/head.php");
+ 
+ ?>
+  <!-- ======= Home Section ======= -->
+
+
+  <!-- ======= Home Section ======= -->
+ <?php
+      require_once("view/partials/extract_vitrine/home.php");
+ 
+ ?>
+  <!-- ======= Home Section ======= -->
+
+
+  <main id="main">
+
+
+
+<!-- ======= A PROPOS Section ======= -->   
+      <?php
+           require_once("view/partials/extract_vitrine/APROPOS.php");
+      ?>
+<!-- ======= A PROPOS Section ======= -->
+
+  <!-- ======= les Ucab Section ======= -->
+     <?php
+        require_once("view/partials/extract_vitrine/ucab.php");
+     ?>
+  <!-- ======= les Ucab Section ======= -->
+  
+
+ <!-- ======= Les filieres pour chaque ucab ======= -->
+ <?php
+        require_once("view/partials/extract_vitrine/filieres.php");
+     ?>
+    <!-- ======= Les filieres pour chaque ucab ======= -->
+   
+
+
+    <!-- ======= Centre Dk Section ======= -->
+    <?php
+        require_once("view/partials/extract_vitrine/equipe.php");
+     ?>
+    <!-- ======= Centre Dk Section ======= -->
+
+   
+    <!-- ======= Commentaire Section ======= -->
+    <?php
+        require_once("view/partials/extract_vitrine/commentaire.php");
+     ?>
+     <!-- ======= Commentaire Section ======= -->
+
+
+    <!-- ======= Contact Section ======= -->
+    <?php
+        require_once("view/partials/extract_vitrine/contact.php");
+     ?>
+   <!-- ======= Contact Section ======= -->
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php
+      require_once("view/partials/extract_vitrine/Footer.php");
+  ?>
+ <!-- ======= Footer ======= -->
+
+
+<!-- ======= Foot ======= -->
+<?php
+    require_once("view/partials/extract_vitrine/foot.php");
+?>
+ <!-- ======= Foot ======= -->
