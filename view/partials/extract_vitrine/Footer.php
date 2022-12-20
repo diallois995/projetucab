727f689b5babc4b4e3a9eff3231df00c13346894
AN200014 <astoumoubayam@gmail.com>
2022-12-20 12:32:05 +0000

    Modification du footer

--- view/partials/extract_vitrine/Footer.php
+++ view/partials/extract_vitrine/Footer.php
@@ -1,5 +1,4 @@
-
- <footer id="footer" style="background-color: #096D1F; color: #fff">
+<footer id="footer" style="background-color: #096D1F; color: #fff">
 
 <div class="footer-newsletter">
   <div class="container">
@@ -18,54 +17,53 @@
 </div>
 
 <div class="footer-top" style="background-color: #096D1F; color: #fff">
-  <div class="container">
-    <div class="row">
-
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-0.10 footer-links text-center border border-danger text-center" style=" background-color: #fff; color: #096D1F ; font-family: 'Merriweather',serif; color: #fff">
-        <h2 class="text-center" style="font-family: 'Merriweather',serif; color: #096D1F"> Menu</h2>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#accu" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">Accueil</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#about" style=" background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">A propos</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#services" style=" background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">Université</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#portfolio" style=" background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">Filiér</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#clients" style=" background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">Partanair</a></li>
-
-        </ul>
-      </div>
-
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-0.10 footer-links border border-danger text-center float-sm-start"  style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">
-        <h2 style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">Campus</h2>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centredk.html" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif" >DAKAR</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centreTouba.html" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">TOUBA</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centreStL.html" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">SAINT-LOUIS</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centreBam.html" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">BAMBEYE</a></li>
-        </ul>
-      </div>
-      
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-0.10 footer-links border border-danger text-center float-sm-start"  style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">
-        <h2 style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">Formations</h2>
-        <ul>
-        <li><i class="bx bx-chevron-right"></i> <a href="#" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif" >FINACE&L'ENSEIGNEMENT</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif" >INFORMATIQUE DE GESTION</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">ELECTROMECANIQUE</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">ADMINISTARATION</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">L'ELEVAGE&AGRICULTURE</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#" style="background-color: #fff; color: #096D1F ;font-family: 'Merriweather',serif">AGROALIMENTAIRE</a></li>
-        </ul>
-      </div>
-      </div>
-      <div style="font-family: 'Merriweather',serif; color: #fff">
-        <div class="social-links mt-3">
-          <a href="#" class="twitter" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-twitter" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
-          <a href="#" class="facebook" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-facebook" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
-          <a href="#" class="instagram" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-instagram" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
-          <a href="#" class="linkedin" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-linkedin" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
+      <div class="container">
+        <div class="row">
+    
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-0.10 footer-links " style="  color: #fff ; font-family: 'Merriweather',serif; color: #fff">
+            <h2 class="" style="font-family: 'Merriweather',serif; color: #fff"> Menu</h2>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#accu" style=" color: #fff ;font-family: 'Merriweather',serif">Accueil</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about" style="  color: #fff ;font-family: 'Merriweather',serif">A propos</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#services" style="  color: #fff ;font-family: 'Merriweather',serif">Université</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#portfolio" style="  color: #fff ;font-family: 'Merriweather',serif">Filiér</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#clients" style="  color: #fff ;font-family: 'Merriweather',serif">Partanair</a></li>
+    
+            </ul>
+          </div>
+    
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-0.10 footer-links  float-sm-start"  style=" color: #fff ;font-family: 'Merriweather',serif">
+            <h2 style=" color: #fff ;font-family: 'Merriweather',serif">Campus</h2>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centredk.html" style=" color:#fff; font-family: 'Merriweather',serif" >DAKAR</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centreTouba.html" style=" color:#fff; font-family: 'Merriweather',serif">TOUBA</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centreStL.html" style=" color:#fff; font-family: 'Merriweather',serif">SAINT-LOUIS</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="public/templates/templateVitrine/centreBam.html" style=" color:#fff; font-family: 'Merriweather',serif">BAMBEY</a></li>
+            </ul>
+          </div>
+          
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-0.10 footer-links float-sm-start"  style=" color: #fff ;font-family: 'Merriweather',serif">
+                  <h2 style=" color: #fff ;font-family: 'Merriweather',serif">Partenaire</h2>
+                  <ul>
+                  <li><i class="bx bx-chevron-right"></i> <a href="#" style=" color: #fff;font-family: 'Merriweather',serif" >Fondation Sonatel</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#" style=" color: #fff;font-family: 'Merriweather',serif" >Université Gaston Berger</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#" style=" color: #fff;font-family: 'Merriweather',serif">Université de Thies </a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#" style=" color: #fff;font-family: 'Merriweather',serif">Diprom</a></li>
+                    <li><i class="bx bx-chevron-right"></i> <a href="#" style=" color: #fff;font-family: 'Merriweather',serif">Institut Africain de Management</a></li>
+                  </ul>
+                </div>
+          </div>
+          <div style="font-family: 'Merriweather',serif; color: #fff">
+            <div class="social-links mt-3">
+              <a href="#" class="twitter" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-twitter" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
+              <a href="#" class="facebook" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-facebook" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
+              <a href="#" class="instagram" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-instagram" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
+              <a href="#" class="linkedin" style="font-family: 'Merriweather',serif; color: #fff"><i class="bx bxl-linkedin" style="font-family: 'Merriweather',serif; color: #fff"></i></a>
+            </div>
+    
         </div>
-
+      </div>
     </div>
-  </div>
-</div>
 
 </footer>
    <!-- ======= UCAB Section ======= -->
